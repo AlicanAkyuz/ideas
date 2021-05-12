c730ae9b424bd202c37b5119285dc6e1487cc972
AlicanAkyuz <alicakyz@gmail.com>
2021-05-12 10:51:20 +0200

    integrate ant design and setup css imports

---
+++ src/App.styles.ts
@@ -0,0 +1,9 @@
+export default {
+  appContainer: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: '100%',
+    height: '100vh',
+  },
+}
--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
 import React, { FC } from 'react'
+import styles from './App.styles'
+import { createUseStyles } from 'react-jss'
+
+const useStyles = createUseStyles(styles)
 
 const App: FC = () => {
+  const classes = useStyles()
+
   return (
-    <div>
+    <div className={classes.appContainer}>
       <h3>Hello World</h3>
     </div>
   )
